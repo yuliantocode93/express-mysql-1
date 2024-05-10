e1f84ed8756f0a07f4197355e3aa8cb5158610da
yulianto <jooliantoo@gmail.com>
2024-05-10 13:51:07 +0700

    update myssql

--- eduwork_cruds_DB.sql
+++ eduwork_cruds_DB.sql
@@ -2,14 +2,20 @@
 create database eduwork_cruds;
 use eduwork_cruds;
 show tables;
+
 insert into products (name, price, stock, status)
 values ('axioo', 15000, 10, 1);
+
 insert into products 
 values (null, 'pongo', 20000, 20, 0);
+
 select * from products;
+
 insert into products 
 values (3, 'zyrex', 10000, 5, 2);
+
 select * from products;
+
 use eduwork_cruds;
 CREATE TABLE users (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -17,6 +23,7 @@ CREATE TABLE users (
     email VARCHAR(50),
     alamat VARCHAR(30) NOT NULL
 );
+
 show tables;
 describe users;
 describe products;
@@ -28,9 +35,15 @@ values (null, "yulianto", "yulianto@gmail.com", "jakarta");
 select * from users;
 delete from products where id = 3;
 select * from products;
-
 update products set stock = 15, status = 0 where id = 1;
 select * from products where status = 0;
 select * from products where name like'Axioo' or status = 1;
+select * from products where name like'%Axioo%' or status = 1;
 
-## JOIN, FULL JOIN, LEFT JOIN, RIGHT JOIN
+## JOIN, FULL JOIN, LEFT JOIN, RIGHT JOIN ##
+select * from products a inner join users b on a.users_id = b.id where b.id = 1;
+select a.*, b.nama from products a inner join users b on a.users_id = b.id where b.id;
+select a.*, b.nama from products a left join users b on a.users_id = b.id;
+
+
+
