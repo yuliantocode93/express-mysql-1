6ea7716647f8917d7a63c0575d54fcb6d298171e
yulianto <jooliantoo@gmail.com>
2024-05-10 17:29:20 +0700

    fixing serelize

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  database: "eduwork_cruds",
+  database: "eduwork-cruds-2",
   host: "localhost",
   username: "root",
   password: "1234",
